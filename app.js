163600bbd2e57cb88957c5d36506d3921162cc3c
Mohand Abdelkader <mohand27m@gmail.com>
2025-05-20 20:22:26 +0300

    updata and delete

--- app.js
+++ app.js
@@ -43,6 +43,38 @@ app.post('/api/v1/tours', (req, res) => {
   );
 });
 
+app.patch('/api/v1/tours/:id', (req, res) => {
+  const id = req.params.id * 1;
+
+  if (id > tours.length) {
+    return res.status(404).json({
+      status: 'fail',
+      message: 'Invalid ID',
+    });
+  }
+  res.status(200).json({
+    status: 'success',
+    data: {
+      tour: 'update tour here',
+    },
+  });
+});
+
+app.delete('/api/v1/tours/:id', (req, res) => {
+  const id = req.params.id * 1;
+
+  if (id > tours.length) {
+    return res.status(404).json({
+      status: 'fail',
+      message: 'Invalid ID',
+    });
+  }
+  res.status(204).json({
+    status: 'success',
+    data: null,
+  });
+});
+
 app.get('/api/v1/tours/:id', (req, res) => {
   const id = req.params.id * 1;
 
