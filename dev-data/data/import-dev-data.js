91367aa967a9abb0fe45ddf0fc52db2c6db61171
Mohand Abdelkader <mohand27m@gmail.com>
2025-05-27 04:16:36 +0300

    script to add data to out database

---
+++ dev-data/data/import-dev-data.js
@@ -0,0 +1,50 @@
+const dotenv = require('dotenv');
+const mongooes = require('mongoose');
+const fs = require('fs');
+const Tour = require('../../models/tourModel');
+
+dotenv.config({ path: './config.env' });
+
+const DB = process.env.DATABASE.replace(
+  '<USERNAME>',
+  process.env.DATABASE_USERNAME,
+).replace('<PASSWORD>', process.env.DATABASE_PASSWORD);
+
+mongooes
+  .connect(DB, {
+    useNewUrlParser: true,
+    useFindAndModify: false,
+    useUnifiedTopology: true,
+  })
+  .then(() => console.log('DB connection successful!'))
+  .catch((err) => console.error('DB connection error:', err));
+
+const tours = JSON.parse(
+  fs.readFileSync(`${__dirname}/tours-simple.json`, 'utf-8'),
+);
+
+const importData = async () => {
+  try {
+    await Tour.create(tours);
+    console.log('Data Successfully loaded');
+    process.exit();
+  } catch (err) {
+    console.log(err);
+  }
+};
+
+const deleteData = async () => {
+  try {
+    await Tour.deleteMany();
+    console.log('DATA Successfully Delete');
+    process.exit();
+  } catch (err) {
+    console.log(err);
+  }
+};
+
+if (process.argv[2] === '--import') {
+  importData();
+} else if (process.argv[2] === '--delete') {
+  deleteData();
+}
