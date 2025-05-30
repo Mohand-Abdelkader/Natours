0802572deda10b68d69ea5290d058199a028ad5b
Mohand Abdelkader <mohand27m@gmail.com>
2025-05-30 03:39:33 +0300

    error controller

--- app.js
+++ app.js
@@ -2,6 +2,8 @@ const express = require('express');
 const morgan = require('morgan');
 const tourRouter = require('./routes/tourRoutes');
 const userRouter = require('./routes/userRoutes');
+const AppError = require('./utils/appError');
+const globalErrorHandler = require('./controllers/errorControllers');
 
 const app = express();
 app.use(express.json());
@@ -19,4 +21,9 @@ app.use((req, res, next) => {
 app.use('/api/v1/tours', tourRouter);
 app.use('/api/v1/users', userRouter);
 
+app.all('*', (req, res, next) => {
+  next(new AppError(`can't find ${req.originalUrl} on this server`, 404));
+});
+
+app.use(globalErrorHandler);
 module.exports = app;
