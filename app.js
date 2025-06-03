8d4398b88e59dbf1b5e78b6593f94ee817415547
Mohand Abdelkader <mohand27m@gmail.com>
2025-06-03 20:20:33 +0300

    rate limit for limit burtforce attack

--- app.js
+++ app.js
@@ -1,18 +1,28 @@
+/* eslint-disable import/no-extraneous-dependencies */
+/* eslint-disable import/order */
 const express = require('express');
 const morgan = require('morgan');
 const tourRouter = require('./routes/tourRoutes');
 const userRouter = require('./routes/userRoutes');
 const AppError = require('./utils/appError');
 const globalErrorHandler = require('./controllers/errorControllers');
+const rateLimit = require('express-rate-limit');
 
 const app = express();
 app.use(express.json());
 app.use(express.static(`${__dirname}/public`));
 
+//GLOBAL MIDDLEWARE
 if (process.env.NODE_ENV === 'development') {
   app.use(morgan('dev'));
 }
 
+const limiter = rateLimit({
+  max: 100,
+  windowMs: 60 * 60 * 1000,
+  message: 'too many requests from this ip, please try again in an hour!',
+});
+app.use('/api', limiter);
 app.use((req, res, next) => {
   req.requestTime = new Date().toISOString();
   // console.log(req.headers);
