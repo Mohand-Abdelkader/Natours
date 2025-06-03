d6a666be6a07d5c636e101c0a55ea94a681b7a8c
Mohand Abdelkader <mohand27m@gmail.com>
2025-06-03 20:40:15 +0300

    Data sanitization

--- app.js
+++ app.js
@@ -8,6 +8,8 @@ const AppError = require('./utils/appError');
 const globalErrorHandler = require('./controllers/errorControllers');
 const rateLimit = require('express-rate-limit');
 const helmet = require('helmet');
+const mongoSanitize = require('express-mongo-sanitize');
+const xss = require('xss-clean');
 
 const app = express();
 
@@ -32,6 +34,12 @@ app.use('/api', limiter);
 
 //Body Parser, reading data from the body into req.body
 app.use(express.json({ limit: '10kb' }));
+
+// Data sanitization against noSql query injection
+app.use(mongoSanitize());
+
+//Data sanitization against xss
+app.use(xss());
 
 // serving static files
 app.use(express.static(`${__dirname}/public`));
