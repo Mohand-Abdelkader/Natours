0d6c08e35d8f5b491fbcc80034612e37a3979c0d
Mohand Abdelkader <mohand27m@gmail.com>
2025-06-15 20:56:53 +0300

    edit the port for deploy

--- server.js
+++ server.js
@@ -20,7 +20,7 @@ mongooes
   })
   .then(() => console.log('DB connection successful!'));
 
-const port = 3000;
+const port = process.env.PORT || 3000;
 const server = app.listen(port, () => {
   console.log(`Server is running on http://localhost:${port}`);
 });
